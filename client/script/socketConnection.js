24c5a5c0f752120baf28419e9a8c2865afdcd2a3
Tom <physik_box@outlook.de>
2023-06-16 14:24:25 +0200

    Test Send Event

--- client/script/socketConnection.js
+++ client/script/socketConnection.js
@@ -1,13 +1,14 @@
-const socket = new WebSocket('ws://localhost:8080/');
+const socket = new WebSocket("ws://localhost:3000");
 
 socket.onopen = function () {
     // Verbindungsaufbau behandeln
     // document.getElementById("a").innerHTML = "Socked Started"
 
 };
+
 socket.onmessage = function(event) {
     console.log(event.data); // Neue Nachricht behandeln
-    document.getElementById("b").innerHTML = event.data;
+    // document.getElementById("b").innerHTML = event.data;
 };
 socket.onclose = function(event) {
     // Verbindungsabbau behandeln
@@ -16,7 +17,21 @@ socket.onclose = function(event) {
 //
 // socket.send("Hello Server");
 //
-// function fire(){
-//     socket.send("+");
-//     console.log("like");
-// }
+function sendEvent(eventName, eventData){
+    const message = {
+        event: eventName,
+        data: eventData,
+    };
+    socket.send(JSON.stringify(message));
+}
+function getValues(){
+    let usr = document.getElementById("usr").value;
+    let pwd = document.getElementById("pwd").value;
+}
+
+
+function fire(){
+
+    sendEvent('login', {username: 'wert1', password: 'wert2'})
+    console.log("like");
+}
