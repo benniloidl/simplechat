405435b877190792dcb2e78210566d5552d17041
MichalskyL <135982693+MichalskyL@users.noreply.github.com>
2023-06-16 14:22:18 +0200

    login

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const ws = require('ws');
-const app = require('express')();
+const express = require('express');
+const app = express();
+const path = require('path');
 const cookieParser = require('cookie-parser');
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("../client"));
 app.use(cookieParser());
 
 const server = app.listen(3000, () => {
@@ -9,35 +13,47 @@ const server = app.listen(3000, () => {
 });
 const wsSrv = new ws.Server({ server });
 
+let sockets = [];
+
+app.get('/', (req, res) => {
+    if(validateUser(req.cookies.username, req.cookies.password) ){
+        res.redirect('/overview');
+    }else{
+    res.sendFile(path.join(__dirname, '../client', 'index.html'));
+    }
+});
+
+app.get('/overview', (req, res) => {
+    if(validateUser(req.cookies.username, req.cookies.password) ){
+        res.sendFile(path.join(__dirname, '../client', 'overview.html'));
+    }else{
+        res.redirect('/overview');
+    }
+});
+
+function validateUser(username, password){
+    return false;
+}
 
 wsSrv.on('connection', (socket) => {
-    socket.on('message', (message) => {
-        const data = JSON.parse(message);
+    sockets.push(socket);
+    socket.on('login', (data) => {
+        console.log(data);
+        console.log(`Login attempt with username: ${data.username} and password: ${data.password}`);
 
-
-        if (data.type === 'login') {
-            const { username, password } = data;
-            console.log(`Login attempt with username: ${username} and password: ${password}`);
-
-            // Erstelle Cookie
-            const cookieValue = { username, password };
-            const cookieOptions = { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 };
-            app.use(express.json());
-            app.use((req, res, next) => {
-                res.cookie('auth', JSON.stringify(cookieValue), cookieOptions);
-                next();
-            });
-
-            //hier die db abfrage
-            if (username === 'test' && password === 'test123') {
-                socket.send(JSON.stringify({ type: 'login', success: true }));
-            } else {
-                socket.send(JSON.stringify({ type: 'login', success: false }));
-            }
+        if(validateUser(username, password)){
+            socket.send('loginAnswer', true);
+        } else {
+            socket.send('loginAnswer', false);
         }
     });
 });
-server.on('close', () => {
+
+wsSrv.on('close', (socket) => {
     console.log('Client disconnected');
+});
+
+server.on('close', () => {
+    console.log('Server closed');
 });
 
