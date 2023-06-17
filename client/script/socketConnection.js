a3ef7f679276e9891d7d0b80706d20b67a0e1a41
Tom <physik_box@outlook.de>
2023-06-17 07:24:53 +0200

    Impemented CSS Variables with a light and Darkmode

--- client/script/socketConnection.js
+++ client/script/socketConnection.js
@@ -27,10 +27,24 @@ function sendEvent(eventName, eventData){
 function getValues(){
     let usr = document.getElementById("usr").value;
     let pwd = document.getElementById("pwd").value;
+
+    // further client side checking
+    if (usr === "" || pwd === ""){
+        return null;
+    }
+    return{
+        username: usr,
+        password: pwd
+    };
 }
 
 
 function fire(){
+    const result = getValues()
+    if (result === null){
+        console.log("Not in format")
+        return;
+    }
 
     sendEvent('login', {username: 'wert1', password: 'wert2'})
     console.log("like");
