f9325fc85d05c6f877f39ceef3cf6e4e63546d4d
MichalskyL <135982693+MichalskyL@users.noreply.github.com>
2023-06-20 16:45:14 +0200

    added manipulation of chatIDs

--- server/server.js
+++ server/server.js
@@ -6,25 +6,41 @@ const cookieParser = require('cookie-parser');
 const { MongoClient } = require('mongodb');
 const uri = "mongodb://localhost:27017/SimpleChat";
 const dbClient = new MongoClient(uri);
-const userCollectionName = "user";
-const chatHistoryCollectionName = "chatHistory";
+let db, user, chatHistory;
+
+let sockets = [];
 
 async function connectToDB() {
     console.log("connecting to db...");
     try {
         await dbClient.connect()
+        db = dbClient.db();
+        user = db.collection("user");
+        chatHistory = db.collection("chatHistory");
     } catch {
         console.error("connecting to db failed");
         process.exit(42);
     }
     console.log("connected successfully\n");
+
+    //test the functions
+    validateUser("test123", "123").then((result) => {
+        console.log("validation: " + result);
+    });
+    createUser("test123", "123").then((result) => {
+        console.log("creating a new user: " + result);
+    });
+    addChatID("test123", "123", "ID1234").then((result) => {
+        console.log("added ID: "+result);
+    });
+    removeChatID("test123", "123", "ID1234").then((result) => {
+        console.log("removed ID: "+result);
+    });
+    getAllChatIDs("test123", "123").then((result) => {
+        console.log(result);
+    });
+    //end of test
 }
-validateUser("test123", "123").then((result) => {
-    console.log("validation: " + result);
-});
-createUser("test123", "123").then((result) => {
-    console.log("creating a new user: " + result);
-});
 
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(cookieParser());
@@ -34,8 +50,6 @@ const server = app.listen(3000, () => {
     connectToDB();
 });
 const wsSrv = new ws.Server({ server });
-
-let sockets = [];
 
 app.get('/', (req, res) => {
     validateUser(req.cookies.username, req.cookies.password).then((result) => {
@@ -56,14 +70,13 @@ app.get('/overview', (req, res) => {
         }
     });
 });
-app.get("*",(_req, res)=>{
+
+app.get("*", (_req, res) => {
     res.redirect('/');
 });
 
 async function validateUser(username, password) {
-    const db = dbClient.db();
-    const user = db.collection(userCollectionName);
-    const result = await user.findOne({ "username": username, "password": password });
+    const result = await user.findOne({ "username": username, "password": password }, { projection: { _id: 1 } });
     if (result) {
         return true;
     } else {
@@ -72,14 +85,49 @@ async function validateUser(username, password) {
 }
 
 async function createUser(username, password) {
-    const db = dbClient.db();
-    const user = db.collection(userCollectionName);
-    const result = await user.findOne({ "username": username });
+    const result = await user.findOne({ "username": username }, { projection: { _id: 1 } });
     if (result) {
         return false;
     } else {
-        await user.insertOne({ "username": username, "password": password });
+        await user.insertOne({ "username": username, "password": password, chats: [] });
         return true;
+    }
+}
+
+async function addChatID(username, password, id) {
+    const valid = await validateUser(username, password);
+    if (valid) {
+        const result = await user.findOne({ "username": username, "chats.id": id }, { projection: { _id: 1 } });
+        if (result) {
+            return "ID already added";
+        }
+        await user.updateOne({ "username": username }, { $push: { chats: { "id": id } } });
+        return true;
+    } else {
+        return "wrong username/password"
+    }
+}
+
+async function removeChatID(username, password, id) {
+    const valid = await validateUser(username, password);
+    if (valid) {
+        const result = await user.findOne({ "username": username, "chats.id": id }, { projection: { _id: 1 } });
+        if (!result) {
+            return "ID not found";
+        }
+        await user.updateOne({ "username": username }, { $pull: { chats: { "id": id } } });
+        return true;
+    } else {
+        return "wrong username/password"
+    }
+}
+
+async function getAllChatIDs(username, password) {
+    const valid = await validateUser(username, password);
+    if (valid) {
+        return await user.findOne({ "username": username }, { projection: { _id: 0, chats: 1 } });
+    } else {
+        return "wrong username/password"
     }
 }
 
@@ -89,11 +137,13 @@ wsSrv.on('connection', (socket) => {
         console.log(data);
         console.log(`Login attempt with username: ${data.username} and password: ${data.password}`);
 
-        if (validateUser(username, password)) {
-            socket.send('loginAnswer', true);
-        } else {
-            socket.send('loginAnswer', false);
-        }
+        validateUser(username, password).then((result) => {
+            if (result) {
+                socket.send('loginAnswer', true);
+            } else {
+                socket.send('loginAnswer', false);
+            }
+        });
     });
 });
 
@@ -101,4 +151,3 @@ server.on('close', () => {
     dbClient.close();
     console.log('Server closed');
 });
-
