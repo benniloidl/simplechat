89757551bf2257b54d56b463fd202d5b52e34f16
Tom <physik_box@outlook.de>
2023-06-28 19:28:55 +0200

    Created BuildChatMessages with example as Test

--- client/script/socketConnection.js
+++ client/script/socketConnection.js
@@ -1,4 +1,8 @@
 const socket = new WebSocket(location.origin.replace(/^http/, 'ws'));
+
+const chatMessageAmount = 10;
+
+
 socket.sendEvent = (eventName, eventData) => {
     const message = {
         event: eventName,
@@ -7,15 +11,11 @@ socket.sendEvent = (eventName, eventData) => {
     socket.send(JSON.stringify(message));
 }
 
-//const chatFunctions = require('./chatConnection')
-//const {chat_clicked} = require("./chatConnection");
-
-//chatFunctions.chat_clicked(socket);
 const fileName = location.href.split("/").slice(-1)
 
 socket.onopen = function () {
-    if(fileName == "dashboard"){
-        socket.sendEvent("fetchChats","");
+    if (fileName == "dashboard") {
+        socket.sendEvent("fetchChats", "");
     }
 };
 
@@ -31,24 +31,112 @@ function loginUser(data) {
 function buildChatOverview(chats) {
     chats.forEach(data => {
 
-    const navigator = document.createElement("div");
-    navigator.classList.add("chat-contact");
-    navigator.onclick = () => {
-        chat_clicked(data.chatId);
-        if (data.type === "user") injectPage("../subpages/dashboard/chat.html");
-        else injectPage("../subpages/dashboard/group.html");
-    }
+        const navigator = document.createElement("div");
+        navigator.classList.add("chat-contact");
+        navigator.onclick = () => {
+            if (data.type === "user") injectPage("../subpages/dashboard/chat.html");
+            else injectPage("../subpages/dashboard/group.html");
+            TESTBUILDCHATMESSAGES();
+            // chat_clicked(data.chatID);
+        }
 
-    const icon = document.createElement("i");
-    icon.classList.add("fas", data.type === "user" ? "fa-user" : "fa-users");
-    navigator.appendChild(icon);
+        const icon = document.createElement("i");
+        icon.classList.add("fas", data.type === "user" ? "fa-user" : "fa-users");
+        navigator.appendChild(icon);
 
-    const name = document.createElement("p");
-    name.innerHTML = data.name;
-    navigator.appendChild(name);
+        const name = document.createElement("p");
+        name.innerHTML = data.name;
+        navigator.appendChild(name);
 
-    document.getElementById("chats").appendChild(navigator);
-});
+        document.getElementById("chats").appendChild(navigator);
+    });
+}
+
+function TESTBUILDCHATMESSAGES() {
+    let testdata = [
+        {
+            message: "message",
+            timestamp: "12 nov 2003 17:01",
+            readConfirmation: true,
+            author: "Honulullu"
+        },
+        {
+            message: "answer",
+            timestamp: "13 nov 2003 17:01",
+            readConfirmation: true,
+            author: "self"
+        },
+        {
+            message: "some other message",
+            timestamp: "28 jun 2023 18:00",
+            readConfirmation: true,
+            author: "Honulullu"
+        },
+        {
+            message: "some other message",
+            timestamp: "28 jun 2023 19:00",
+            readConfirmation: true,
+            author: "Honulullu"
+        },
+    ]
+    // try {
+    //     buildChatMessages(testdata, true);
+    // } catch (e) {
+    //     setTimeout(() => buildChatMessages(testdata, true), 1000);
+    // }
+    setTimeout(() => buildChatMessages(testdata, true), 10);
+
+}
+
+function buildChatMessages(chatMessages, group) {
+    const chatBox = document.createElement("div");
+    chatBox.id = "chat-box";
+    let lastAuthor;
+    chatMessages.forEach(data => {
+        console.log(data);
+        const chatElement = document.createElement("div");
+        chatElement.classList.add("chat-element");
+        chatElement.classList.add(data.author === "self" ? "chat-element-right" : "chat-element-left");
+
+        if (group === true && lastAuthor !== data.author) {
+            if (data.author !== "self") {
+                const senderElement = document.createElement("span");
+                senderElement.classList.add("sender");
+                senderElement.innerHTML = data.author;
+                chatElement.appendChild(senderElement);
+            }
+            lastAuthor = data.author;
+        }
+
+
+        const messageElement = document.createElement("p");
+        messageElement.innerHTML = data.message;
+        chatElement.appendChild(messageElement);
+
+        const timeElement = document.createElement("span");
+        let messageDate = new Date(data.timestamp); // bspw: "28 Jun 2023 18:50:59"
+        let timeDifference = Math.floor((Date.now() - messageDate.valueOf()) / 1000 / 60)
+        if (timeDifference < 15) {
+            timeElement.innerHTML = timeDifference.toString() + "min ago";
+        } else if (timeDifference < 60 * 24) {
+            timeElement.innerHTML = `${messageDate.getHours()}:${messageDate.getMinutes()}`
+            timeElement.innerHTML = messageDate.toLocaleTimeString("en-UK", {hour: '2-digit', minute: '2-digit'});
+        } else {
+            timeElement.innerHTML = messageDate.toLocaleString();
+        }
+        timeElement.classList.add("subtitle");
+        chatElement.appendChild(timeElement);
+
+        // TODO style and insert read indicator
+        const readIndicator = document.createElement("span");
+        readIndicator.innerHTML = "READELEMENT";
+        // chatElement.appendChild(readIndicator);
+
+        chatBox.appendChild(chatElement);
+    });
+    // document.getElementById("chat")
+    let a = document.getElementById("chat-box");
+    document.getElementById("chat-box").replaceWith(chatBox);
 }
 
 socket.onmessage = function (event) {
@@ -64,6 +152,12 @@ socket.onmessage = function (event) {
                 setTimeout(() => buildChatOverview(data.chats), 1000);
             }
             break;
+        case 'chatMessages':
+            try {
+                buildChatMessages(data.content, data.isgroup)
+            } catch (e) {
+                setTimeout(() => buildChatMessages(data.chats, data.isgroup), 1000);
+            }
     }
 };
 
@@ -118,4 +212,26 @@ function loginRequest() {
     }
 
     socket.sendEvent('login', {username: result.username, password: result.password})
+}
+
+function chat_clicked(socket, chatId) {
+    console.log("selected chat: ", chatId)
+    socket.sendEvent('fetch-chat-message', {
+        chatID: chatId,
+        start: 0,
+        amount: chatMessageAmount
+    });
+}
+
+function chat_scrolled(socket, chatId) {
+    let times = 2;
+    socket.sendEvent('fetch-chat-message', {
+        chatID: chatId,
+        start: (chatMessageAmount * times),
+        amount: (chatMessageAmount * (times + 1))
+    });
+}
+
+function chat_overview(socket) {
+    socket.sendEvent('fetchChats', {})
 }
--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongo = require('mongodb');
-const { MongoClient } = mongo;
+const {MongoClient} = mongo;
 const uri = "mongodb://127.0.0.1:27017/SimpleChat";
 const dbClient = new MongoClient(uri);
 let db, user, chatHistory;
@@ -18,13 +18,13 @@ async function connectToDB() {
         process.exit(42);
     }
     console.log("connected successfully\n");
-    /*createChat("Chat123", "user").then((result) => {
+    /*createChat("user", "user").then((result) => {
         console.log("createChat: " + result);
         addChat("Test12345", result).then((result) => {
             console.log("added ID: " + result);
         });
-    });
-    fetchChats("Test12345").then((result) => {
+    });*/
+    /*fetchChats("Test12345").then((result) => {
         console.log(result);
     });
     //test the functions
@@ -56,7 +56,7 @@ async function connectToDB() {
 }
 
 async function validateUser(username, password) {
-    const result = await user.findOne({ "username": username, "password": password }, { projection: { _id: 1 } });
+    const result = await user.findOne({"username": username, "password": password}, {projection: {_id: 1}});
     if (result) {
         return true;
     } else {
@@ -65,39 +65,39 @@ async function validateUser(username, password) {
 }
 
 async function createUser(username, password) {
-    const result = await user.findOne({ "username": username }, { projection: { _id: 1 } });
+    const result = await user.findOne({"username": username}, {projection: {_id: 1}});
     if (result) {
         return false;
     } else {
-        await user.insertOne({ "username": username, "password": password, chats: [] });
+        await user.insertOne({"username": username, "password": password, chats: []});
         return true;
     }
 }
 
 async function addChat(username, chatID) {
-    const result = await user.findOne({ "username": username, "chats.chatID": chatID }, { projection: { _id: 1 } });
+    const result = await user.findOne({"username": username, "chats.chatID": chatID}, {projection: {_id: 1}});
     if (result) {
         return false;
     }
-    await user.updateOne({ "username": username }, { $push: { chats: { "chatID": chatID, "unreadMessages": 0 } } });
+    await user.updateOne({"username": username}, {$push: {chats: {"chatID": chatID, "unreadMessages": 0}}});
     return true;
 }
 
 async function removeChat(username, chatID) {
-    const result = await user.findOne({ "username": username, "chats.chatID": chatID }, { projection: { _id: 1 } });
+    const result = await user.findOne({"username": username, "chats.chatID": chatID}, {projection: {_id: 1}});
     if (!result) {
         return false;
     }
-    await user.updateOne({ "username": username }, { $pull: { chats: { "chatID": chatID } } });
+    await user.updateOne({"username": username}, {$pull: {chats: {"chatID": chatID}}});
     return true;
 }
 
 async function getAllChatIDs(username) {
-    return await user.findOne({ "username": username }, { projection: { _id: 0, chats: 1 } });
+    return await user.findOne({"username": username}, {projection: {_id: 0, chats: 1}});
 }
 
 async function getChatDetails(chatID) {
-    return await chatHistory.findOne({ "_id": new mongo.ObjectId(chatID) }, { projection: { _id: 0, name: 1, type: 1 } });
+    return await chatHistory.findOne({"_id": new mongo.ObjectId(chatID)}, {projection: {_id: 0, name: 1, type: 1}});
 }
 
 async function fetchChats(username) {
@@ -107,7 +107,7 @@ async function fetchChats(username) {
         for (let i = 0; i < chatIDs.chats.length; i++) {
             const id = chatIDs.chats[i];
             const detail = await getChatDetails(id.chatID);
-            chats.push({ "chatID": id.chatID, "name": detail.name, "type": detail.type });
+            chats.push({"chatID": id.chatID, "name": detail.name, "type": detail.type});
         }
     } else {
         return false;
@@ -116,12 +116,12 @@ async function fetchChats(username) {
 }
 
 async function createChat(name, type) {
-    const result = await chatHistory.insertOne({ "name": name, "type": type, "messages": [] });
+    const result = await chatHistory.insertOne({"name": name, "type": type, "messages": []});
     return result.insertedId.toString();
 }
 
 async function loadMessages(chatID, start, amount) {
-    const result = await chatHistory.findOne({ "_id": new mongo.ObjectId(chatID) }, { projection: { _id: 0, messages: 1 } });
+    const result = await chatHistory.findOne({"_id": new mongo.ObjectId(chatID)}, {projection: {_id: 0, messages: 1}});
     if (result) {
         let ret;
         try {
@@ -140,9 +140,9 @@ async function loadMessages(chatID, start, amount) {
 }
 
 async function addMessage(chatID, message) {
-    const result = await chatHistory.findOne({ "_id": new mongo.ObjectId(chatID) }, { projection: { _id: 1 } });
+    const result = await chatHistory.findOne({"_id": new mongo.ObjectId(chatID)}, {projection: {_id: 1}});
     if (result) {
-        await chatHistory.updateOne({ "_id": new mongo.ObjectId(chatID) }, { $push: { messages: message } });
+        await chatHistory.updateOne({"_id": new mongo.ObjectId(chatID)}, {$push: {messages: message}});
         return true;
     } else {
         return false;
