7519570272a231e21ecb49929b99ca4b4bc14efc
Benjamin Loidl <benni@loidlnetz.de>
2023-07-04 09:40:59 +0200

    Read Confirmation

--- client/script/dashboard.js
+++ client/script/dashboard.js
@@ -2,22 +2,22 @@
  * Overwrites and generate the overview header for groups
  * @param users
  */
-function createViewContainer(users){
+function createViewContainer(users) {
     console.log("users", users)
     const ul = document.createElement("ul");
-
+    
     for (const user of users) {
         let userObject = generateUsers(user);
         ul.appendChild(userObject);
     }
-
+    
     document.querySelector(".overview-container ul").replaceWith(ul);
     document.querySelector(".overview-container button").addEventListener("click", () => {
         let username = getCookie("username");
         removeUser(username);
     });
-
-
+    
+    
 }
 
 /**
@@ -25,20 +25,22 @@ function createViewContainer(users){
  * @param username
  * @returns {HTMLLIElement}
  */
-function generateUsers(username){
+function generateUsers(username) {
     const element = document.createElement("li");
     const user = document.createElement("i");
     const minus = document.createElement("i");
-
+    
     user.classList.add("fas", "fa-user");
     minus.classList.add("fas", "fa-minus");
-
+    
     element.appendChild(user);
     element.innerHTML = username;
     element.appendChild(minus);
-
-    minus.addEventListener("click", () => {removeUser(username)})
-
+    
+    minus.addEventListener("click", () => {
+        removeUser(username)
+    })
+    
     return element;
 }
 
@@ -48,7 +50,7 @@ function generateUsers(username){
  */
 function buildChatOverview(data) {
     const chats = data.chats;
-    if(!chats){
+    if (!chats) {
         return;
     }
     
@@ -59,27 +61,27 @@ function buildChatOverview(data) {
         navigator.classList.add("chat-contact");
         navigator.setAttribute("data-chat-id", data.chatID);
         navigator.setAttribute("chatType", data.type);
-
+        
         // notification
         if (unreadMessages > 0) {
             navigator.classList.add("notification");
         }
-
+        
         // events
         navigator.onclick = () => {
             loadChat(data, navigator);
         }
-
+        
         // icon
         const icon = document.createElement("i");
         icon.classList.add("fas", data.type === "user" ? "fa-user" : "fa-users");
         navigator.appendChild(icon);
-
+        
         // chat name
         const name = document.createElement("p");
         name.innerHTML = data.name;
         navigator.appendChild(name);
-
+        
         document.getElementById("chats").appendChild(navigator);
     });
 }
@@ -91,16 +93,16 @@ function buildChatOverview(data) {
  * @param data
  * @param navigator
  */
-function loadChat(data, navigator){
+function loadChat(data, navigator) {
     /* Workaround: If template has to load, execution has to wait till elements are loaded.
     Otherwise, Script tries to access not existing element
      */
-
-    const path = (data.type === "user")? "../subpages/dashboard/chat.html":"../subpages/dashboard/group.html";
+    
+    const path = (data.type === "user") ? "../subpages/dashboard/chat.html" : "../subpages/dashboard/group.html";
     injectPageAsync(path, () => {
         sessionStorage.setItem("openedChat", data.chatID.toString())
         chat_fetchMessage(socket, data.chatID);
-
+        
         if (elementHasNotification(navigator)) {
             chat_read_event(socket, data.chatID);
             navigator.classList.remove("notification");
@@ -128,25 +130,25 @@ function buildChatMessages(chatData) {
     const chatBox = document.createElement("div");
     chatBox.id = "chat-box";
     sessionStorage.setItem("lastAuthor", null);
-
+    
     // build chat messages
     chatData.messages.forEach(data => {
         let chatElement = buildMessageObject(data, chatData.username, type);
         chatBox.appendChild(chatElement);
     });
-
+    
     // Arrange Items in Container
-    if(type==="group"){
+    if (type === "group") {
         chat_get_group_users(socket, chatData.chatID);
     } else {
-        document.querySelector(".chat-contact div").addEventListener("click", () =>{
+        document.querySelector(".chat-contact div").addEventListener("click", () => {
             let username = getCookie("username");
             removeUser(username);
         });
     }
     document.getElementById("chat-box").replaceWith(chatBox);
     document.getElementById("chat-name").innerHTML = name;
-
+    
     // EventListener
     document.getElementById("submit-message").onclick = () => {
         sendMessage(chatData.chatID);
@@ -171,7 +173,7 @@ function buildMessageObject(messageObject, username, type) {
     const chatElement = document.createElement("div");
     chatElement.classList.add("chat-element");
     chatElement.classList.add(messageObject.author === username ? "chat-element-right" : "chat-element-left");
-
+    
     // Sender information (only relevant in groups)
     if (type === 'group' && lastAuthor !== messageObject.author) {
         if (messageObject.author !== username) {
@@ -182,12 +184,12 @@ function buildMessageObject(messageObject, username, type) {
         }
         sessionStorage.setItem("lastAuthor", messageObject.author);
     }
-
+    
     // message
     const messageElement = document.createElement("p");
     messageElement.innerHTML = messageObject.message;
     chatElement.appendChild(messageElement);
-
+    
     // timestamp
     const timeElement = document.createElement("span");
     let messageDate = new Date(messageObject.timeStamp); // eg: "28 Jun 2023 18:50:59"
@@ -199,30 +201,40 @@ function buildMessageObject(messageObject, username, type) {
     }
     timeElement.classList.add("subtitle");
     chatElement.appendChild(timeElement);
-
+    
     /** time calculation to get order
-        issue: order has max value of 2147483647
-        calculations to fit datetime into this size:
-    */
-    // resolution 1 second, 20 Years, reference 29 june 2023
-    // let modifiedTime = Math.round((messageDate.valueOf()/1000) -  1000000000 -  688000000);
-    // resolution 0.1 second 4 Years selected
+     issue: order has max value of 2147483647
+     calculations to fit datetime into this size:
+     */
+        // resolution 1 second, 20 Years, reference 29 june 2023
+        // let modifiedTime = Math.round((messageDate.valueOf()/1000) -  1000000000 -  688000000);
+        // resolution 0.1 second 4 Years selected
     let modifiedTime1 = Math.round((messageDate.valueOf() / 100) - 10000000000 - 6880000000);
     chatElement.style.order = modifiedTime1.toString();
     // console.log(messageDate.valueOf(),modifiedTime, modifiedTime1 , chatElement.style.order);
-
+    
     // read confirmation
     // TODO reading confirmation style should be done by @Benni
-    if(messageObject.readConfirmation === true){
-        chatElement.classList.add("read-confirmation");
+    if (messageObject.readConfirmation === true) {
+        readMessage(chatElement);
     }
     return chatElement;
 }
 
-function markChatAsRead(){
+function markChatAsRead() {
     const elements = document.querySelectorAll("#chat-box .chat-element-right")
     for (const element of elements) {
-        element.classList.add("read-confirmation")
+        readMessage(element);
+    }
+}
+
+function readMessage(element) {
+    const className = "read-confirmation";
+    if (!element.classList.contains(className)) {
+        element.classList.add(className);
+        const eye = document.createElement("i");
+        eye.classList.add("fas", "fa-eye", "read-confirmation-icon");
+        element.appendChild(eye);
     }
 }
 
@@ -259,7 +271,7 @@ function notificationHandler(notification) {
         chat_read_event(socket, notification.chatID);
         return;
     }
-
+    
     // Notification style
     if (chatNode) {
         chatNode.classList.remove("notification");
@@ -268,7 +280,7 @@ function notificationHandler(notification) {
         chatNode.classList.add("notification");
         // console.log(chatNode);
     }
-
+    
 }
 
 /**
@@ -292,17 +304,17 @@ function elementHasNotification(element) {
 function injectPageAsync(url, execution) {
     const main = document.querySelector('main');
     if (main !== undefined) main.setAttribute('data-menu-open', 'false');
-
+    
     const chatDiv = document.getElementById("chat");
     const xhr = new XMLHttpRequest();
-
+    
     xhr.open("GET", "dashboard/" + url, true);
-    xhr.onreadystatechange =  () => {
+    xhr.onreadystatechange = () => {
         if (xhr.readyState === 4 && xhr.status === 200) {
             chatDiv.innerHTML = xhr.responseText;
-
+            
             execution()
-
+            
             document.querySelectorAll(".username").forEach(function (element) {
                 element.innerHTML = getCookie("username");
             });
@@ -316,24 +328,24 @@ function injectPageAsync(url, execution) {
  * return false to disable form
  * @returns {false}
  */
-function addUserToGroup(){
+function addUserToGroup() {
     const field = document.getElementById("add-member");
-    if(!field) return false;
+    if (!field) return false;
     let chatID = sessionStorage.getItem("openedChat");
-    if(getChatNodeById(chatID).getAttribute("chatType") !== "group") return false;
-
+    if (getChatNodeById(chatID).getAttribute("chatType") !== "group") return false;
+    
     let username = field.value.trim();
     console.log(username)
-    if(!checkUsernameSemantic(username)) {
-       showError("Username must only contain upper- and lowercase " +
+    if (!checkUsernameSemantic(username)) {
+        showError("Username must only contain upper- and lowercase " +
             "letters, digits and the special characters \\+\\-\\_\\.");
         console.error("username doesn't match", username)
         return false;
-    } else{
+    } else {
         showError("");
         field.value = "";
     }
-
+    
     chat_addUser(username);
     return false;
 }
@@ -349,7 +361,7 @@ function addUserToGroup(){
  */
 function injectFileWithForm(path, formEventFunction, parameter) {
     sessionStorage.clear();
-    injectPageAsync(path, () =>{
+    injectPageAsync(path, () => {
         let form = document.forms[0];
         form.addEventListener("submit", (event) => {
             event.preventDefault();
@@ -358,7 +370,7 @@ function injectFileWithForm(path, formEventFunction, parameter) {
     });
 }
 
-function serverConnectionLost(){
+function serverConnectionLost() {
     const element = document.createElement("div");
     const wrapper = document.createElement("div");
     const heading = document.createElement("h1");
@@ -366,28 +378,29 @@ function serverConnectionLost(){
     const button = document.createElement("button");
     heading.textContent = "You lost connection with our server!";
     button.textContent = "Reload";
-    text.textContent ="Reconnect will be attempted in 5 seconds."
-
+    text.textContent = "Reconnect will be attempted in 5 seconds."
+    
     wrapper.appendChild(heading);
     wrapper.appendChild(text);
     wrapper.appendChild(button);
-
+    
     // wrapper.innerHTML = "some Text ";
     element.classList.add("missingConnection");
-
+    
     element.appendChild(wrapper);
     document.body.appendChild(element);
+    
     // document.body.replaceWith(element)
-
-    function timer(){
+    
+    function timer() {
         setTimeout(() => {
             console.log("timer");
-           window.location.reload();
-           timer();
+            window.location.reload();
+            timer();
         }, 5000);
     }
-
+    
     button.addEventListener("click", () => window.location.reload());
     timer();
-
+    
 }
